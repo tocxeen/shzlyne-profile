6144b55279f7cf753dd957491247c2b84ee38ff2
tocxeen <richard@clicknpay.africa>
2023-11-02 23:17:04 +0200

    skills,work experience and contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import Corousel from "../component/Corousel";
 import Toolbar from "../component/Toolbar";
 import About from "../component/About";
+import Skills from "../component/Skills";
+import WorkExperience from "../component/WorkExperience";
+import Contact from "../component/Contact";
 
 const Home = () => {
   return (
@@ -8,6 +11,9 @@ const Home = () => {
       <Toolbar />
       <Corousel />
       <About />
+      <Skills />
+      <WorkExperience />
+      <Contact />
     </div>
   );
 };
